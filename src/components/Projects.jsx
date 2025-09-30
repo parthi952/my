0f0f5ca530aabf9f0808868638673fbc97567859
parthimp952 <parthimp952@gmail.com>
2025-09-30 18:13:20 +0530

    some to chane

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,20 +6,20 @@ function Projects() {
         {
             title: "KiteCareer-Chatbot",
             description: "📌 KiteCareer Chatbot 📖 Overview The KiteCareer Chatbot is a Python-based AI assistant designed to provide quick and friendly responses about the company’s details, services, and technologies. It integrates Google Generative AI (Gemini) with NLTK for text tokenization and Pandas for structured data handling.",
-            image: "src/assets/project/chat.jpeg",
+            image: "src/assets/chat.jpg",
             link: "https://github.com/parthi952/-KiteCareer-Chatbot.git"
         },
         {
             title: "Flipkart-Product-Scraper",
             description: "📌 Flipkart-Product-Scraper 📖 This Python project automates the process of scraping product information from Flipkart based on a search term. It retrieves data such as product names and prices, and organizes it into a structured Excel file for easy analysis.",
-            image: "src/assets/project/download.jpeg",
+            image: "src/assets/flipkart.jpg",
             link: "https://github.com/parthi952/Flipkart-Product-Scraper.git"
         },
         {
             title: "Birthday-Bot-Chatbot",
             description: "📌 Birthday-Bot-Chatbot 📖 This Python bot reads from an Excel sheet, matches current dates to birthdays, and sends personalized email greetings automatically.",
-            image: "src/assets/project/bot.jpeg",
-            link: "https://github.com/parthi952/Birthday-Bot-Chatbot.git"
+            image: "src/assets/bot.jpg",
+            link: "https://github.com/parthi952/Birthday-Bot-Chat.git"
         }
     ]
 
